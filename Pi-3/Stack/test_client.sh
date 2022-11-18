3618264c4bdb47dc7313249f89e2f38219db39f1
Group 26 <group26@soton.ac.uk>
2022-11-18 11:59:34 +0000

    Corrected loop for stack usage testing

--- Pi-3/Stack/test_client.sh
+++ Pi-3/Stack/test_client.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in {0..6} 
+for i in {0..5} 
 do 
-    for j in {0..5} 
+    for j in {0..6} 
     do
         sleep 5
         valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./client $i $j
