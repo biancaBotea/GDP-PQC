2f55b779fa09e8a6d92f12783c05cf17c049841a
biancaBotea <botea.bianca1@gmail.com>
2022-11-29 14:43:36 +0000

    new cbd

--- Pi-Pico/mbedtls/library/saber_cbd.c
+++ Pi-Pico/mbedtls/library/saber_cbd.c
@@ -7,10 +7,8 @@ Vadim Lyubashevsky, John M. Schanck, Peter Schwabe & Damien stehle
 ----------------------------------------------------------------------*/
 
 #include "pq/saber_api.h"
-#include<stdint.h>
-#include<stdio.h>//added
+#include <stdint.h>
 #include "pq/saber_params.h"
-#include <immintrin.h>//added
 
 static uint64_t load_littleendian(const unsigned char *x, int bytes)
 {
@@ -53,83 +51,30 @@ void cbd(uint16_t *r, const unsigned char *buf)
 
   }
 #elif Saber_type == 2
-
+  uint32_t t,d, a[4], b[4];
   int i,j;
-  uint32_t t1[8];//there will be 8 32 bit integer
-  uint32_t d1[4][8];//there will be 8 32 bit integer
 
-  __m256i const_avx, d_avx, t_avx, temp_avx, mask_const_avx, a_avx[4], b_avx[4];
-
-//----------------setting the constants----------------
-  __m256i mask;
-  unsigned long long int mask_ar[4];
-  mask_ar[0]=~(0UL);mask_ar[1]=~(0UL);mask_ar[2]=~(0UL);mask_ar[3]=~(0UL);
-  mask = _mm256_loadu_si256 ((__m256i const *)mask_ar);
-
-
-  const_avx= _mm256_set1_epi32 (0x11111111);
-  mask_const_avx= _mm256_set1_epi32 (0xf);
-
-//----------------setting the constants ends----------------
-
-  for(i=0;i<SABER_N/(4*8);i++)
+  for(i=0;i<SABER_N/4;i++)
   {
-   
-	for(j=0;j<8;j++){	
-	    t1[j] = load_littleendian(buf+32*i+4*j,4);
-	}
+    t = load_littleendian(buf+4*i,4);
+    d = 0;
+    for(j=0;j<4;j++)
+      d += (t >> j) & 0x11111111;
 
-	d_avx=_mm256_xor_si256 (d_avx, d_avx);//make d=0
+    a[0] =  d & 0xf;
+    b[0] = (d >>  4) & 0xf;
+    a[1] = (d >>  8) & 0xf;
+    b[1] = (d >> 12) & 0xf;
+    a[2] = (d >> 16) & 0xf;
+    b[2] = (d >> 20) & 0xf;
+    a[3] = (d >> 24) & 0xf;
+    b[3] = (d >> 28);
 
-    t_avx=_mm256_loadu_si256 ((__m256i const *) (&t1));//load t	
-
-	for(j=0;j<4;j++){
-		temp_avx= _mm256_srli_epi32 (t_avx, j);
-		temp_avx=_mm256_and_si256 (temp_avx, const_avx);
-		d_avx=_mm256_add_epi32 (d_avx, temp_avx);
-		
-	} 
-
-
-	a_avx[0]= _mm256_and_si256(d_avx, mask_const_avx);
-	temp_avx= _mm256_srli_epi32(d_avx,4);
-	b_avx[0]=_mm256_and_si256 (temp_avx, mask_const_avx);
-
-	temp_avx= _mm256_srli_epi32(d_avx,8);
-	a_avx[1]=_mm256_and_si256 (temp_avx, mask_const_avx);
-	temp_avx= _mm256_srli_epi32(d_avx,12);
-	b_avx[1]=_mm256_and_si256 (temp_avx, mask_const_avx);
-
-	temp_avx= _mm256_srli_epi32(d_avx,16);
-	a_avx[2]=_mm256_and_si256 (temp_avx, mask_const_avx);
-	temp_avx= _mm256_srli_epi32(d_avx,20);
-	b_avx[2]=_mm256_and_si256 (temp_avx, mask_const_avx);
-
-	temp_avx= _mm256_srli_epi32(d_avx,24);
-	a_avx[3]=_mm256_and_si256 (temp_avx, mask_const_avx);
-	b_avx[3]= _mm256_srli_epi32(d_avx,28);
-	
-	//--------------------------------
-	
-	a_avx[0]=_mm256_sub_epi32 (a_avx[0], b_avx[0]);
-	a_avx[1]=_mm256_sub_epi32 (a_avx[1], b_avx[1]);
-	a_avx[2]=_mm256_sub_epi32 (a_avx[2], b_avx[2]);
-	a_avx[3]=_mm256_sub_epi32 (a_avx[3], b_avx[3]);
-
-	//--------------place the values-------
-	_mm256_maskstore_epi32 ((int *)d1[0], mask, a_avx[0]);
-	_mm256_maskstore_epi32 ((int *)d1[1], mask, a_avx[1]);
-	_mm256_maskstore_epi32 ((int *)d1[2], mask, a_avx[2]);
-	_mm256_maskstore_epi32 ((int *)d1[3], mask, a_avx[3]);
-	
-	for(j=0;j<8;j++){	
-		r[4*(8*i+j)+0]=d1[0][j] &  Qmod_minus1;
-		r[4*(8*i+j)+1]=d1[1][j] &  Qmod_minus1;
-		r[4*(8*i+j)+2]=d1[2][j] &  Qmod_minus1;
-		r[4*(8*i+j)+3]=d1[3][j] &  Qmod_minus1;
-	}
+    r[4*i+0] = (uint16_t)(a[0]  - b[0]) & Qmod_minus1;
+    r[4*i+1] = (uint16_t)(a[1]  - b[1]) & Qmod_minus1;
+    r[4*i+2] = (uint16_t)(a[2]  - b[2]) & Qmod_minus1;
+    r[4*i+3] = (uint16_t)(a[3]  - b[3]) & Qmod_minus1;
   }
-
 #elif Saber_type == 1
   uint64_t t,d, a[4], b[4];
   int i,j;
