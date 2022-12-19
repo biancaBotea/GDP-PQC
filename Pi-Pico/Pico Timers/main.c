34a7958dbda913655b6434e1f2382758097fbc54
benjaminaheath <benjaheath@gmail.com>
2022-12-19 17:59:11 +0000

    move demo_utime to header

--- Pi-Pico/Pico Timers/main.c
+++ Pi-Pico/Pico Timers/main.c
@@ -3,35 +3,10 @@
 #include "utime.h"
 #include "systick.h"
 
-void demo_utime(){
-    microsecond_count_t* us_500 = (microsecond_count_t*) malloc(sizeof(microsecond_count_t));
-    microsecond_count_t* us_1000 = (microsecond_count_t*) malloc(sizeof(microsecond_count_t));
-
-    init_utime(us_500);
-    init_utime(us_1000);
-
-    begin_utime(us_500);
-    begin_utime(us_1000);
-    for(int a = 0; a<3; ++a){
-        busy_wait_ms(500);
-        split_utime(us_500);
-        busy_wait_ms(500);
-        split_utime(us_500);
-        split_utime(us_1000);
-    }
-    busy_wait_ms(500);
-    end_utime(us_500);
-    busy_wait_ms(500);
-    end_utime(us_1000);
-
-    print_utime(us_500);
-    print_utime(us_1000);
-}
-
 int main(){
     //Init stdio Output
     stdio_init_all();
 
-    demo_utime();
+    // demo_utime();
     return 0;
 }
--- Pi-Pico/Pico Timers/utime.h
+++ Pi-Pico/Pico Timers/utime.h
@@ -94,3 +94,28 @@ void print_utime(microsecond_count_t* us_ct){
         printf("%llu\n",us_ct->ut_diffs[d]);
     }
 }
+
+void demo_utime(){
+    microsecond_count_t* us_500 = (microsecond_count_t*) malloc(sizeof(microsecond_count_t));
+    microsecond_count_t* us_1000 = (microsecond_count_t*) malloc(sizeof(microsecond_count_t));
+
+    init_utime(us_500);
+    init_utime(us_1000);
+
+    begin_utime(us_500);
+    begin_utime(us_1000);
+    for(int a = 0; a<3; ++a){
+        busy_wait_ms(500);
+        split_utime(us_500);
+        busy_wait_ms(500);
+        split_utime(us_500);
+        split_utime(us_1000);
+    }
+    busy_wait_ms(500);
+    end_utime(us_500);
+    busy_wait_ms(500);
+    end_utime(us_1000);
+
+    print_utime(us_500);
+    print_utime(us_1000);
+}
