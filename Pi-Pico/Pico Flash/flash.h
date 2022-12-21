53fa7ac7b74bd77b19811329ddd99f97ecee59f3
benjaminaheath <benjaheath@gmail.com>
2022-12-21 13:05:19 +0000

    flash initial commit

---
+++ Pi-Pico/Pico Flash/flash.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+#include "hardware/flash.h"
+
---
+++ Pi-Pico/Pico Flash/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "flash.h"
+
+int main(){
+    stdio_init_all();
+    printf("Hello World!");
+    return 0;
+}
