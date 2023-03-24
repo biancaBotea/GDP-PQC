1a470f320b0cdf3df3eb8cf9c18b11e31d81a745
Tom Gibson <tjg1g19@soton.ac.uk>
2023-03-24 13:35:48 +0000

    Removed old pico code

--- Pi-Pico/Benchmarks/pico_client.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLIENT_HEADER_H
-# define CLIENT_HEADER_H
-
-#include "lwip/altcp_tcp.h"
-
-int run_client(const ip_addr_t server_ip, const char *cert, char *MsgToServer);
-
-#endif
--- Pi-Pico/Pico Flash/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "flash.h"
-
-int main(){
-    stdio_init_all();
-    int ret;
-    ret = demo_flasharray();
-    return ret;
-}
