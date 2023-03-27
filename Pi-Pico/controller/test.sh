9ec54aa97251723f6cab208eac2587d06146763e
Tom Gibson <tjg1g19@soton.ac.uk>
2023-03-27 16:16:07 +0100

    refactored script to improve readability and console output

--- Pi-Pico/controller/test.sh
+++ Pi-Pico/controller/test.sh
@@ -7,9 +7,27 @@ flash_pico () {
 	sudo rm -r build
 	mkdir build
 	cd build
-	cmake .. -DWIFI_SSID="TOMS LAPTOP" -DWIFI_PASSWORD="12345678" ..
-	cmake --build --config Release .
-	sudo openocd -f interface/picoprobe.cfg -f target/rp2040.cfg -c "program latency_benchmark_client.elf verify reset exit"
+	echo "Building pico-sdk and client application"
+	cmake .. -DWIFI_SSID="TOMS LAPTOP" -DWIFI_PASSWORD="12345678" .. &> /dev/null
+	cmake --build --config Release . &> /dev/null
+	echo "Flashing Pico"
+	sudo openocd -f interface/picoprobe.cfg -f target/rp2040.cfg -c "program latency_benchmark_client.elf verify reset exit" &> /dev/null
+}
+
+reset_pico () {
+	echo "Resetting Pico - Starting test"
+	sudo openocd -f interface/picoprobe.cfg -f target/rp2040.cfg -c "init; reset; exit" &> /dev/null
+}
+
+log_test () {
+	cd $WRK_DIR
+	echo "Initialising Logging"
+	sudo python3 dmm_control.py -l on -f k${1}d${2}t1.csv
+	python3 ./termination_server.py
+	echo "Terminating Logging"
+	sudo python3 dmm_control.py -l off
+	echo "Test Finished"
+	echo ""
 }
 
 rm results.txt
@@ -20,79 +38,63 @@ do
 	echo "Kyber & Saber l$j"
 	for k in 2 3 5
 	do
-		cd $WRK_DIR		
+		cd $WRK_DIR
+		echo "Copying files"
 		cp ./config/kyber_params_l$j.h $PICO_SDK_PATH/lib/mbedtls/include/pq/kyber_params.h
 		cp ./config/saber_params_l$j.h $PICO_SDK_PATH/lib/mbedtls/include/pq/saber_params.h
 		cp ./config/dilithium_params_l$k.h $PICO_SDK_PATH/lib/mbedtls/include/pq/dilithium_params.h
 		cp ./config/new_certs_l$k.h $WRK_DIR/Benchmarks/new_certs.h
 
 		# Give Server time to compile mbed
-		sleep 30
+		echo "Waiting for server to compile Mbed TLS"
+		sleep 28
 		
 		echo "Dilithium l$k"
 		# Flash pico once with dilithium client then run it another 2 times by resetting device with breaks in between to allow for server to restart/recompile?
 		cd $WRK_DIR
 		cp ./config/dilithium_client.c $WRK_DIR/Benchmarks/Latency/client.c
+		
 		flash_pico
-		cd $WRK_DIR
-		sudo python3 dmm_control.py -l on -f k${j}d${k}t1.csv
-		python3 ./termination_server.py
-		sudo python3 dmm_control.py -l off
+		log_test $j $k
 
-		sudo openocd -f interface/picoprobe.cfg -f target/rp2040.cfg -c "init; reset; exit"
-		sudo python3 dmm_control.py -l on -f k${j}d${k}t2.csv
-		python3 ./termination_server.py
-		sudo python3 dmm_control.py -l off
+		reset_pico
+		log_test $j $k
 
-		sudo openocd -f interface/picoprobe.cfg -f target/rp2040.cfg -c "init; reset; exit"
-		sudo python3 dmm_control.py -l on -f k${j}d${k}t3.csv
-		python3 ./termination_server.py
-		sudo python3 dmm_control.py -l off
+		reset_pico
+		log_test $j $k
 	done
 
 	echo "Sphincs"
 	cd $WRK_DIR
 	cp ./config/sphincs_client.c $WRK_DIR/Benchmarks/Latency/client.c
+	
 	flash_pico
-	sudo python3 dmm_control.py -l on -f k${j}d${k}t4.csv
-	cd $WRK_DIR
-	python3 ./termination_server.py
-	sudo python3 dmm_control.py -l off
+	log_test $j $k
 
-	sudo openocd -f interface/picoprobe.cfg -f target/rp2040.cfg -c "init; reset; exit"
-	sudo python3 dmm_control.py -l on -f k${j}d${k}t5.csv
-	python3 ./termination_server.py
-	sudo python3 dmm_control.py -l off
+	reset_pico
+	log_test $j $k
 
 	echo "ECDSA"
 	cd $WRK_DIR
 	cp ./config/ecdsa_client.c $WRK_DIR/Benchmarks/Latency/client.c
+	
 	flash_pico
-	sudo python3 dmm_control.py -l on -f k${j}d${k}t6.csv
-	cd $WRK_DIR
-	python3 ./termination_server.py
-	sudo python3 dmm_control.py -l off
+	log_test $j $k
 
-	sudo openocd -f interface/picoprobe.cfg -f target/rp2040.cfg -c "init; reset; exit"
-	sudo python3 dmm_control.py -l on -f k${j}d${k}t7.csv
-	python3 ./termination_server.py
-	sudo python3 dmm_control.py -l off
+	reset_pico
+	log_test $j $k
 	
 done
 
 echo "ECDHE with Sphincs or ECDSA"
 cd $WRK_DIR
 cp ./config/sphincs_client.c $WRK_DIR/Benchmarks/Latency/client.c
+
 flash_pico
-sudo python3 dmm_control.py -l on -f k${j}d${k}t8.csv
-cd $WRK_DIR
-python3 ./termination_server.py
-sudo python3 dmm_control.py -l off
+log_test $j $k
 
 cd $WRK_DIR
 cp ./config/ecdsa_client.c $WRK_DIR/Benchmarks/Latency/client.c
+
 flash_pico
-sudo python3 dmm_control.py -l on -f k${j}d${k}t9.csv
-cd $WRK_DIR
-python3 ./termination_server.py
-sudo python3 dmm_control.py -l off
+log_test $j $k
--- Pi-Pico/tls_server/test.sh
+++ Pi-Pico/tls_server/test.sh
@@ -2,6 +2,15 @@
 
 WRK_DIR="`pwd`"
 
+run_test () {
+	sleep 2
+	echo "Starting test"
+	./server $1
+	python3 ./termination_client.py
+	echo "Test finished"
+	echo ""
+}
+
 cd $WRK_DIR
 
 for j in 1 3 5
@@ -10,6 +19,7 @@ do
 	for k in 5
 	do
 		sleep 2
+		echo "Copying files"
 		cp ./test_config/kyber_params_l$j.h $MBEDTLS_PATH/include/pq/kyber_params.h
 		cp ./test_config/saber_params_l$j.h $MBEDTLS_PATH/include/pq/saber_params.h
 		cp ./test_config/dilithium_params_l$k.h $MBEDTLS_PATH/include/pq/dilithium_params.h
@@ -19,50 +29,34 @@ do
 		sudo rm -rf build
 		mkdir build
 		cd build
-		cmake .. -DENABLE_TESTING=OFF ..
-		cmake --build .
-		sudo cmake --install .
+		echo "Building Mbed TLS"
+		cmake .. -DENABLE_TESTING=OFF .. &> /dev/null
+		cmake --build . &> /dev/null
+		echo "Installing Mbed TLS"
+		sudo cmake --install . &> /dev/null
 
 		cd $WRK_DIR
 
 		echo "Dilithium L$k"
 		gcc server.c ./ssl_server.c -lmbedtls -lmbedx509 -lmbedcrypto -lm -o server
-		./server 6
-		python3 ./termination_client.py
-
-		sleep 2
-		./server 7
-		python3 ./termination_client.py
 		
-		sleep 2
-		./server 8
-		python3 ./termination_client.py
+		run_test 6
+		run_test 7
+		run_test 8
 	done
 	
 	echo "Sphincs"
-	sleep 2
-	./server 4
-	python3 ./termination_client.py
+	run_test 4
 	
-	sleep 2
-	./server 5
-	python3 ./termination_client.py
+	run_test 5
 	
 	echo "ECDSA"
-	sleep 2
-	./server 1
-	python3 ./termination_client.py
+	run_test 1
 	
-	sleep 2
-	./server 2
-	python3 ./termination_client.py
+	run_test 2
 done
 
 echo "ECDHE"
-sleep 2
-./server 3
-python3 ./termination_client.py
+run_test 3
 
-sleep 2
-./server 0
-python3 ./termination_client.py
+run_test 0
