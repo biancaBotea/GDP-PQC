5b0ff6a7b563ebf9874f5e0bd4d77af3e3ab1d51
Tom Gibson <tjg1g19@soton.ac.uk>
2023-03-29 18:37:26 +0100

    Removed function to make it compatible with new version of pico-sdk

--- Pi-Pico/controller/Benchmarks/Power/pico_client.c
+++ Pi-Pico/controller/Benchmarks/Power/pico_client.c
@@ -28,27 +28,6 @@ typedef struct TLS_CLIENT_T_ {
 
 static struct altcp_tls_config *tls_config = NULL;
 
-
-/* Function to feed mbedtls entropy. May be better to move it to pico-sdk */
-int mbedtls_hardware_poll(void *data, unsigned char *output, size_t len, size_t *olen) {
-    /* Code borrowed from pico_lwip_random_byte(), which is static, so we cannot call it directly */
-    static uint8_t byte;
-
-    for(int p=0; p<len; p++) {
-        for(int i=0;i<32;i++) {
-            // picked a fairly arbitrary polynomial of 0x35u - this doesn't have to be crazily uniform.
-            byte = ((byte << 1) | rosc_hw->randombit) ^ (byte & 0x80u ? 0x35u : 0);
-            // delay a little because the random bit is a little slow
-            busy_wait_at_least_cycles(30);
-        }
-        output[p] = byte;
-    }
-
-    *olen = len;
-    return 0;
-}
-
-
 static err_t tls_client_close(void *arg) {
     TLS_CLIENT_T *state = (TLS_CLIENT_T*)arg;
     err_t err = ERR_OK;
