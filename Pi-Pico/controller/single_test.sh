060dfca371d5724a8c9dbb1a98cfb50d002bc770
Tom Gibson <tjg1g19@soton.ac.uk>
2023-04-26 17:03:06 +0100

    Added ability to change test params

--- Pi-Pico/controller/single_test.sh
+++ Pi-Pico/controller/single_test.sh
@@ -4,6 +4,17 @@ WRK_DIR="`pwd`"
 debug=1
 test_name="Power"
 
+j=1
+k=2
+cert_alg="dilithium"
+
+echo "Copying files"
+cp ./Benchmarks/$test_name/config/kyber_params_l$j.h $PICO_SDK_PATH/lib/mbedtls/include/pq/kyber_params.h
+cp ./Benchmarks/$test_name/config/saber_params_l$j.h $PICO_SDK_PATH/lib/mbedtls/include/pq/saber_params.h
+cp ./Benchmarks/$test_name/config/dilithium_params_l$k.h $PICO_SDK_PATH/lib/mbedtls/include/pq/dilithium_params.h
+cp ./Benchmarks/$test_name/config/new_certs_l$k.h $WRK_DIR/Benchmarks/$test_name/new_certs.h
+cp ./Benchmarks/$test_name/config/${cert_alg}_client.c $WRK_DIR/Benchmarks/$test_name/client.c
+
 cd $WRK_DIR/Benchmarks/$test_name
 sudo rm -r build
 mkdir build
