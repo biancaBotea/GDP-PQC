a3ba6e2d55ac348e0b15eeb9520c52f85a2057c3
Tom Gibson <tjg1g19@soton.ac.uk>
2023-04-28 13:29:33 +0100

    Updated pi stack tests to use more efficient testing flow

--- Pi-3/MbedTLS/Stack/test_client.sh
+++ Pi-3/MbedTLS/Stack/test_client.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 WRK_DIR="`pwd`"
+debug=0
+
+process_results () {
+	bash get_results.sh |& tee -a ./results.txt
+	rm massif*
+}
 
 cd $WRK_DIR
 
 for j in 1 3 5
 do
+	echo "Kyber & Saber L$j" |& tee -a ./results.txt
 	for k in 2 3 5
 	do
 		sleep 5
@@ -18,15 +25,47 @@ do
 		sudo rm -rf build
 		mkdir build
 		cd build
-		cmake .. -DENABLE_TESTING=OFF ..
-		cmake --build .
-		sudo cmake --install .
+		then
+			cmake .. -DENABLE_TESTING=OFF ..
+			cmake --build .
+			sudo cmake --install .
+		else
+			cmake .. -DENABLE_TESTING=OFF .. &> /dev/null
+			cmake --build . &> /dev/null
+			sudo cmake --install . &> /dev/null
+		fi
 
 		cd $WRK_DIR
 
+		echo "Dilithium L$k" |& tee -a ./results.txt
 		gcc client.c ../ssl_client1.c -lmbedtls -lmbedx509 -lmbedcrypto -lm -o client
-		bash loop_client.sh
-		bash get_results.sh #|& tee ./results/$j$j$k.txt
-		rm massif*
+    	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./client 6
+		process_results
+		sleep 2
+    	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./client 7
+		process_results
+		sleep 2
+    	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./client 8
+		process_results
 	done
+	echo "Sphincs" |& tee -a ./results.txt
+	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./client 4
+	process_results
+	sleep 2
+	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./client 5
+	process_results
+	sleep 2
+	echo "ECDSA" |& tee -a ./results.txt
+	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./client 1
+	process_results
+	sleep 2
+	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./client 2
+	process_results
 done
+echo "ECDHE - Sphincs" |& tee -a ./results.txt
+valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./client 3
+process_results
+sleep 2
+echo "ECDHE - Sphincs" |& tee -a ./results.txt
+valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./client 0
+process_results
--- Pi-3/MbedTLS/Stack/test_server.sh
+++ Pi-3/MbedTLS/Stack/test_server.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 WRK_DIR="`pwd`"
+debug=0
+
+process_results () {
+	bash get_results.sh |& tee -a ./results.txt
+	rm massif*
+}
 
 cd $WRK_DIR
 
 for j in 1 3 5
 do
+	echo "Kyber & Saber L$j" |& tee -a ./results.txt
 	for k in 2 3 5
 	do
-		sleep 2
+		sleep 5
 		cp ../test_config/kyber_params_l$j.h $MBEDTLS_PATH/include/pq/kyber_params.h
 		cp ../test_config/saber_params_l$j.h $MBEDTLS_PATH/include/pq/saber_params.h
 		cp ../test_config/dilithium_params_l$k.h $MBEDTLS_PATH/include/pq/dilithium_params.h
@@ -18,15 +25,41 @@ do
 		sudo rm -rf build
 		mkdir build
 		cd build
-		cmake .. -DENABLE_TESTING=OFF ..
-		cmake --build .
-		sudo cmake --install .
+		then
+			cmake .. -DENABLE_TESTING=OFF ..
+			cmake --build .
+			sudo cmake --install .
+		else
+			cmake .. -DENABLE_TESTING=OFF .. &> /dev/null
+			cmake --build . &> /dev/null
+			sudo cmake --install . &> /dev/null
+		fi
 
 		cd $WRK_DIR
 
+		echo "Dilithium L$k" |& tee -a ./results.txt
 		gcc server.c ../ssl_server.c -lmbedtls -lmbedx509 -lmbedcrypto -lm -o server
-		bash loop_server.sh
-		bash get_results.sh #|& tee ./results/$j$j$k.txt
-		rm massif*
+    	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./server 6
+		process_results
+    	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./server 7
+		process_results
+    	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./server 8
+		process_results
 	done
+	echo "Sphincs" |& tee -a ./results.txt
+	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./server 4
+	process_results
+	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./server 5
+	process_results
+	echo "ECDSA" |& tee -a ./results.txt
+	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./server 1
+	process_results
+	valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./server 2
+	process_results
 done
+echo "ECDHE - Sphincs" |& tee -a ./results.txt
+valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./server 3
+process_results
+echo "ECDHE - Sphincs" |& tee -a ./results.txt
+valgrind --tool=massif --heap=no --stacks=yes --detailed-freq=100 ./server 0
+process_results
